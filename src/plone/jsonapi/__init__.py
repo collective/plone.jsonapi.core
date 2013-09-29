9f56458bc3a81139110d994a9a874469b7720cbe
Ramon Bartl <ramon_bartl@yahoo.de>
2013-09-29 11:57:20 +0200

    added version route

--- src/plone/jsonapi/__init__.py
+++ src/plone/jsonapi/__init__.py
@@ -5,6 +5,21 @@
 __author__ = 'Ramon Bartl <ramon.bartl@googlemail.com>'
 __docformat__ = 'plaintext'
 
+import pkg_resources
+
 from browser import router
+
+
+def version():
+    dist = pkg_resources.get_distribution("plone.jsonapi")
+    return dist.version
+
+
+@router.add_route("/version", "apiversion", methods=["GET"])
+def apiversion(context, request):
+    return {
+        "url": router.url_for("apiversion", force_external=True),
+        "version": version()
+    }
 
 # vim: set ft=python ts=4 sw=4 expandtab :
