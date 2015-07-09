fa94abc99c9a2d82aab3f2e51916391bf89564cc
Ramon Bartl <rb@ridingbytes.com>
2015-07-09 15:02:27 +0200

    updated version route to display build and date

--- src/plone/jsonapi/core/version.py
+++ src/plone/jsonapi/core/version.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'Ramon Bartl <ramon.bartl@googlemail.com>'
-__docformat__ = 'plaintext'
-
 import pkg_resources
 from browser import router
+
+__author__ = 'Ramon Bartl <ramon.bartl@googlemail.com>'
+__docformat__ = 'plaintext'
 
 
 def version():
     dist = pkg_resources.get_distribution("plone.jsonapi.core")
     return dist.version
 
+
+__version__ = version()
+__build__ = 42
+__date__ = '2015-07-09'
+
+
 @router.add_route("/version", "apiversion", methods=["GET"])
 def apiversion(context, request):
     return {
-        "url": router.url_for("apiversion", force_external=True),
-        "version": version()
+        "url":     router.url_for("apiversion", force_external=True),
+        "version": __version__,
+        "build":   __build__,
+        "date":    __date__,
     }
-
-# vim: set ft=python ts=4 sw=4 expandtab :
